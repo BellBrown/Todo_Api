8ef7eee201141fa8b3346c39e5b6424fc569b1cf
BellBrown <94917218+BellBrown@users.noreply.github.com>
2021-12-17 10:46:15 +0000

    todo API

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { request, response } = require('express');
 const express = require ('express');
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT|| 3000;
 // importing mongoose
 const mongoose = require('mongoose');
 // importing todoController
@@ -8,15 +10,19 @@ const todoController = require('./controllers/todoController')
 
 app.use(express.json());
 
+app.get('/',(request,response)=>{
+    response.status(200).json({message:"Hello Welcome to my todo API"});
+});
+
 app.post('/todo',todoController.addTodo);
 app.get('/todo',todoController.getAllTodo);
 app.patch('/todo/:todoId',todoController.updateById);
 app.delete('/todo/:todoId',todoController.deleteById);
 app.get('/todo/:todoId',todoController.getTodoById);
 
-app.listen(3000,()=>{
-    console.log("The server is running on port 3000");
-    mongoose.connect('mongodb+srv://Christabel:1234@cluster0.vzylt.mongodb.net/todo_db?retryWrites=true&w=majority')
+app.listen(port,()=>{
+    console.log("The server is running on port 3000",);
+    mongoose.connect(process.env.DB_URL)
     .then(function(){
         console.log("DataBase is connected");
     })
